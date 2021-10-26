67594a68e2b8ccd7111a2c2970ab2294f8a830b1
Caio <caiovictorsalesdefreitas@gmail.com>
2021-10-26 12:57:55 -0300

    Autenticação Parte 1 e 2

--- market/__init__.py
+++ market/__init__.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template
 # Importando a instância do flask
 from flask_sqlalchemy import SQLAlchemy
 # Importando a instância do SQL Alchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_manager
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '5f5e3ad8db84c9e5a33938ab'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
