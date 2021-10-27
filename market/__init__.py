738662e075fa9ff126ed32e3146f44828739bdf7
Caio <caiovictorsalesdefreitas@gmail.com>
2021-10-27 15:19:46 -0300

    Logout Custom

--- market/__init__.py
+++ market/__init__.py
@@ -11,5 +11,7 @@ app.config['SECRET_KEY'] = '5f5e3ad8db84c9e5a33938ab'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from market import routes
