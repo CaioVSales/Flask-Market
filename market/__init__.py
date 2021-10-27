6276948bdccc8b0484dcad50e4ee24a378622dd3
Caio <caiovictorsalesdefreitas@gmail.com>
2021-10-27 18:38:33 -0300

    Reformulação Geral

--- market/__init__.py
+++ market/__init__.py
@@ -1,17 +1,14 @@
-from flask import Flask, render_template
-# Importando a instância do flask
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# Importando a instância do SQL Alchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, login_manager
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SECRET_KEY'] = '5f5e3ad8db84c9e5a33938ab'
+app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
-
 from market import routes
